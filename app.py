c4a8b06a3904090b37b73f516b53c77363deab45
vvams@Vamsi_krishna <vvamsikrishnakoneit@gmail.com>
2026-05-25 14:28:34 +0530

    new exicution

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv("C:\\Users\\vvams\\OneDrive\\Desktop\\exf server monitering\\logging_monitoring_anomalies.csv")
-df
+print(df.head())
+print(df.info())
+print(df.isnull().sum())
